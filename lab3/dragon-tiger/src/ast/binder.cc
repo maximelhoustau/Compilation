b08ddaef3234f215e96d575be75bedca1c6b9b4b
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-05-29 18:21:57 +0200

    Corrections de bugs n°2

--- lab3/dragon-tiger/src/ast/binder.cc
+++ lab3/dragon-tiger/src/ast/binder.cc
@@ -242,8 +242,9 @@ void Binder::visit(ForLoop &loop) {
 
 void Binder::visit(Break &b) {
 	b.set_loop(parentloops.back());
-	if(parentloops.empty() || parentloops.back())
+	if(parentloops.empty() || parentloops.back() == nullptr)
 		error("Break is at a wrong place");
+	b.set_loop(parentloops.back());
 }
 
 void Binder::visit(Assign &assign) {
