9af1182875385497fe0863ef287a712d888c91c9
maximelhoustau <maxime.lhoustau@telecom-paristech.fr>
2019-06-14 11:36:20 +0200

    Whileloop done, BinaryOpe done

--- lab3/dragon-tiger/src/ast/type_checker.cc
+++ lab3/dragon-tiger/src/ast/type_checker.cc
@@ -21,10 +21,25 @@ void TypeChecker::visit(StringLiteral &str){
 }
 
 void TypeChecker::visit(BinaryOperator &op){
-	
+	op.get_left().accept(*this);
+	Type type_op_left = op.get_left().get_type();
+        op.get_right().accept(*this);	
+	Type type_op_right = op.get_right().get_type();
+	if(type_op_left == type_op_right){
+		Operator ope = op.op;
+		if(type_op_left == t_int)
+			op.set_type(t_int);
+		if(type_op_left == t_string && (ope == o_eq || ope == o_neq || ope == o_ge || ope == o_le))
+			op.set_type(t_string);
+		else
+			error("Operation not permitted with srings");
+	}
+	else
+		error("Operation of two different type objects");
 }
 
 void TypeChecker::visit(Sequence &seq){
+	//Prend le type de la derniere expr
 	std::vector<Expr *> exprs = seq.get_exprs();
 	Expr* last_expr = exprs[(int)exprs.size()-1];
 	last_expr->accept(*this);
@@ -36,6 +51,7 @@ void TypeChecker::visit(Sequence &seq){
 }
 
 void TypeChecker::visit(Let &let){
+	//Prend le type de la derniere expr
 	std::vector<Decl *> decls = let.get_decls();
       	Decl* last_decl = decls[(int)decls.size()-1];
 	last_decl->accept(*this);
@@ -58,6 +74,7 @@ void TypeChecker::visit(IfThenElse &ite){
 	Type type_then = ite.get_then_part().get_type();
 	ite.get_else_part().accept(*this);
 	Type type_else = ite.get_else_part().get_type();
+	//Type du if doit etre int, ceux du then et du else doivent etre les meme
 	if(type_if == t_int && type_then == type_else)
 		ite.set_type(type_then);
 	else
@@ -81,17 +98,18 @@ void TypeChecker::visit(VarDecl &decl){
 	decl.accept(*this);
 	optional<Expr &> expr = decl.get_expr();
 	optional<Symbol> type = decl.type_name;
+	//Type défini, on vérifie que le type déclaré est le meme que le type de la variable
 	if(type){
 		if(symbol_to_type(*type) == expr->get_type())
 			decl.set_type(expr->get_type());
 	}
+	//Type non défini, on attribue le type de la variable
 	else{
 		Type type_expr = expr->get_type();
 		if(type_expr == t_int || type_expr  == t_string)
 			decl.set_type(type_expr);
 		else
-			error("Variable type must be integer or string, not void");
-		
+			error("Variable type must be integer or string, not void");		
 	}
 }
 
@@ -103,16 +121,25 @@ void TypeChecker::visit(FunCall &){
 
 }
 
-void TypeChecker::visit(WhileLoop &){
-
+void TypeChecker::visit(WhileLoop &loop){
+	loop.get_condition().accept(*this);
+	Type type_cond = loop.get_condition().get_type();
+	loop.get_body().accept(*this);
+	Type type_body = loop.get_body().get_type();
+	if(type_body == t_void && type_cond == t_int)
+		loop.set_type(t_void);
+	else
+		error("While loops must have a voided body and an integer condition");
 }
 
 void TypeChecker::visit(ForLoop &){
 
 }
 
-void TypeChecker::visit(Break &){
-
+void TypeChecker::visit(Break &br){
+	//Breaks sont void
+	br.accept(*this);
+	br.set_type(t_void);
 }
 
 void TypeChecker::visit(Assign &){
